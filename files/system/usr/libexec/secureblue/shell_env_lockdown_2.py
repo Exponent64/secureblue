0d9fac15bf5db8dd39a3b73837e6dee0ea5024b9
Exponent64 <259167702+Exponent64@users.noreply.github.com>
2026-08-12 19:07:58 +0000

    .

--- files/system/usr/libexec/secureblue/shell_env_lockdown_2.py
+++ files/system/usr/libexec/secureblue/shell_env_lockdown_2.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 # SPDX-FileCopyrightText: Copyright 2026 The Secureblue Authors
 #
@@ -6,7 +6,7 @@
 
 import sys
 from pathlib import Path
-from subprocess import CalledProcessError
+from subprocess import CalledProcessError, run
 from typing import assert_never
 
 from utils import (
@@ -89,12 +89,52 @@ def lockdown_status() -> None:
             print(f"{file} {Colors.red}does not exist{Colors.end}")
 
 
-# def lock_files(shell: str, file: str) -> None:
-#     path: Path = Path(file)
-#
-#     if file.endswith("/"):
-#         path.mkdir(mode=600, parents=True, exist_ok=True)
-#         run(["/usr/bin/chattr", "+i", file], check=True)
+def unlock_files() -> None:
+
+    print("Unlocking files...")
+    try:
+        run(["/usr/bin/run0"], check=True)
+    except CalledProcessError:
+        print("Root permissions not granted. Exiting...")
+        return
+
+    for file in target_files():
+        print(f"Unlocking {file}...")
+        if str(file).endswith("/"):
+            run(["/usr/bin/chattr", "-R", "-i", file], check=True)
+        else:
+            run(["/usr/bin/chattr", "-i", file], check=True)
+
+    print("Files unlocked.")
+
+
+def lock_files() -> None:
+
+    user_input: str = "YES I UNDERSTAND"
+
+    print("""WARNING: This will lock all the configuration files for your installed shells.
+             This is needed to ensure the mitigation (see LD_PRELOAD attacks) is effective.""")
+
+    if input(f"Type {user_input} and press Enter to continue: ") != user_input:
+        print("Wrong user input received. Exiting...")
+
+    print("Locking files...")
+    try:
+        run(["/usr/bin/run0"], check=True)
+    except CalledProcessError:
+        print("Root permissions not granted. Exiting...")
+        return
+
+    for file in target_files():
+        print(f"Locking {file}...")
+        if str(file).endswith("/"):
+            file.mkdir(mode=600, parents=True, exist_ok=True)
+            run(["/usr/bin/chattr", "-R", "+i", file], check=True)
+        else:
+            file.touch(mode=600, exist_ok=True)
+            run(["/usr/bin/chattr", "+i", file], check=True)
+
+    print("Files locked.")
 
 
 def main() -> int:
@@ -105,17 +145,21 @@ def main() -> int:
         print(f"Usage error: {e}. See usage with --help.")
         return 2
 
-    match mode:
-        case ToggleMode.ON:
-            print("This is a test.")
-        case ToggleMode.OFF:
-            print("This is a test.")
-        case ToggleMode.STATUS:
-            lockdown_status()
-        case ToggleMode.HELP:
-            print("Help")
-        case _ as unreachable:
-            assert_never(unreachable)
+    try:
+        match mode:
+            case ToggleMode.ON:
+                lock_files()
+            case ToggleMode.OFF:
+                unlock_files()
+            case ToggleMode.STATUS:
+                lockdown_status()
+            case ToggleMode.HELP:
+                print("Help")
+            case _ as unreachable:
+                assert_never(unreachable)
+    except CalledProcessError:
+        print("An error occurred.")
+        return 1
 
     return 0
 
